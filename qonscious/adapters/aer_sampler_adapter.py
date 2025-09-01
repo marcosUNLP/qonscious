acd5100f0af1ecb3e88ca2d87c57977e558813ba
Alejandro Fernandez <alejandro.casco.fernandez@gmail.com>
2025-08-31 21:16:18 -0300

    added T1, T2 and n_qubits to adapters

--- qonscious/adapters/aer_sampler_adapter.py
+++ qonscious/adapters/aer_sampler_adapter.py
@@ -20,10 +20,22 @@ class AerSamplerAdapter(BackendAdapter):
         self.sampler = sampler or Sampler()
 
     @property
-    def max_qubits(self) -> int:
+    def n_qubits(self) -> int:
         "Estimates the maximum number of qubits this computer can simulate"
         "considering the available memory and some rules of thumb"
         return int(math.log2(psutil.virtual_memory().available / 16))
+
+    @property
+    def t1s(self) -> dict[int, float]:
+        "In an aer simulator, there is no limit on the t1."
+        "It could be different if we include a noise model"
+        return {qubit: float("inf") for qubit in range(self.n_qubits)}
+
+    @property
+    def t2s(self) -> dict[int, float]:
+        "In an aer simulator, there is no limit on the t2."
+        "It could be different if we include a noise model"
+        return {qubit: float("inf") for qubit in range(self.n_qubits)}
 
     def run(self, circuit: QuantumCircuit, **kwargs) -> ExperimentResult:
         shots = kwargs.get("shots", 1024)
@@ -51,9 +63,3 @@ class AerSamplerAdapter(BackendAdapter):
             },
             "raw_results": job.result(),
         }
-
-    @property
-    def t1s(self) -> dict[int, float]:
-        "In an aer simulator, there is no limit on the t1."
-        "It could be different if we include a noise model"
-        return {qubit: float("inf") for qubit in range(self.max_qubits)}
